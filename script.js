900607f929db5ee92b102fade190500a309fed39
Emperor Code <ksaisamhith@gmail.com>
2021-03-24 00:46:31 +0000

    Some stuff

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("holder");
 var ctx = canvas.getContext("2d");
 
+console.log(screen.height,screen.width)
+
+
 function Tank(x,y){
 	this.x = x;
 	this.y = y;
@@ -15,24 +18,26 @@ function Bullet(x,y){
 Tank.prototype.draw = function () {
 	var tank = new Image();
 	tank.src = 'https://art.pixilart.com/8e7d871b88dbc81.gif'; 
-	ctx.clearRect(0,0,canvas.width,canvas.height)
-	ctx.drawImage(tank,this.x,this.y,60,40)
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.drawImage(tank,this.x,this.y,60,40);
 }
 
  Tank.prototype.moveDown = function () {
-   this.y += .5;
+   this.y += 2;
  }
 
  Tank.prototype.moveRight = function () {
-   this.x += .5;
+   this.x += 2;
  }
 
  Tank.prototype.moveLeft = function () {
-   this.x -= .5;
+   this.x -= 2;
  }
  Tank.prototype.moveUp = function () {
-	 this.y -= .5;
+	 this.y -= 2;
  }
+
+
 
  Bullet.prototype.draw = function () {
 	 ctx.arc(this.x,this.y,2,0,2 * Math.PI)
@@ -44,35 +49,48 @@ Tank.prototype.draw = function () {
  }
 
  Tank.prototype.shoot = function () {
-	 
 	 this.bullets.push(new Bullet(this.x + 60, this.y + 21.5));
  }
 
- 
+ Tank.prototype.touchingwall = function () {
+	 if(this.y <= -16.75){
+		 this.y += 20;
+	 }
+
+	 if(this.y >= 115){
+		 this.y -= 20;
+	 }
+
+	 if(this.x <= -16.75){
+		 this.x += 20;
+	 }
+
+	 if(this.x >= 245) {
+		 this.x -= 20;
+	 }
+
+	 this.tank.draw()
+ }
 
 function Game(){
 	this.tank = new Tank(10,10);
 	document.addEventListener("keydown",(e) => {
 		var x = e.keyCode;
-		if(x == 40){
+		if(x == 83 || x == 40){
 			this.tank.moveDown();
 			this.tank.draw();
-		}else if(x == 39){
+		}else if(x == 68 || x == 39){
      this.tank.moveRight();
 		 this.tank.draw();
-		}else if(x == 38){
+		}else if(x == 87 || x == 38){
 			this.tank.moveUp();
 			this.tank.draw();
-		}else if(x == 37){
+		}else if(x == 65 || x == 37){
 			this.tank.moveLeft();
 			this.tank.draw();
 		}else if(x == 32) {
       this.tank.shoot()
-		}
-
-		
-	
-	  
+		} 
 	});
 }
 
@@ -80,8 +98,7 @@ Game.prototype.run = function () {
 	this.interval = setInterval(function(){
 		ctx.clearRect(0,0,canvas.width,canvas.height)
 		this.tank.draw();
-		
-	
+		this.tank.touchingwall()
     for (var i = 0; i < this.tank.bullets.length; i++) {
 			this.tank.bullets[i].draw();
       this.tank.bullets[i].fire();
@@ -91,46 +108,11 @@ Game.prototype.run = function () {
       this.tank.bullets.shift();  
     }
 
+		
+
 	}.bind(this),100)
 }
 
 var game = new Game();
 
 game.run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
