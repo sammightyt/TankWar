903ee7806d183445d2263fff53438c7fde140395
Sai Samhith Kota <>
2021-04-06 20:09:52 +0000

    AI Drawn

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-# TankWar
+# TankWars&trade; Development
 
-***js
-console.log("I boss")
+TankWars&trade; is currently in development. The Alpha stage will release on April 9th as stated by SamSur Programming, the developer. For more info, contact either [suryabhattiprol](https://replit.com/@suryabhattiprol) or [sammightyt](https://replit.com/@sammightyt).
+
 ***
+
+## Overview
+
+Creation Date: March 23, 2021
+
+Version 1.1: Tank drawn
+
+Version 1.2: Tank can move
+
+Version 1.3: Tank can shoot with bugs
+
+Version 1.4: Shooting bugs fixed
+
+Version 2.1: Start Screen added
+
+Version 2.2: Ammo added with bugs
+
+Version 2.3: Ammo bugs fixed
+
+Version 2.4: Ammo Reload added
+
+Version 2.5: Health added
+
+Version 2.6: Wall crash = health lose added
+
+Version 2.7: End message added
+
+Version 2.8: Badges Added
+
+Version 3.1: AI Tank Drawn
+
+Version 3.2: In Development
+
+```js
+var aI = new Tank()
+```
+
+&copy; 2021 SamSur Programming
+
+TankWars&trade; is a registered trademark of SamSur Programming
--- script.js
+++ script.js
@@ -1,118 +1,198 @@
 var canvas = document.getElementById("holder");
 var ctx = canvas.getContext("2d");
+var stage = "version 3.1"
+var game = new Game();
+var enemyX = game.tank.x;
+var enemyY = game.tank.y;
 
-console.log(screen.height,screen.width)
-
-
-function Tank(x,y){
+function Tank(x, y, which) {
 	this.x = x;
 	this.y = y;
 	this.bullets = [];
+	this.tankimg = new Image();
+	this.tankimg.src = 'https://art.pixilart.com/8e7d871b88dbc81.gif';
+	this.tankimg2 = new Image();
+	this.tankimg2.src = "tankIN.png"
+	this.ammo = 15;
+	this.health = 100;
+	this.message = "You Died"
+	this.which = which;
+
 }
 
-function Bullet(x,y){
- this.x = x;
- this.y = y;
+Tank.prototype.moveEnemy = function () {
+
+  this.y = enemyY;
+
+}
+
+function Bullet(x, y) {
+	this.x = x;
+	this.y = y;
+	this.damage = 5;
 }
 
 Tank.prototype.draw = function () {
-	var tank = new Image();
-	tank.src = 'https://art.pixilart.com/8e7d871b88dbc81.gif'; 
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.drawImage(tank,this.x,this.y,60,40);
+	if (this.which == 1) {
+		ctx.drawImage(this.tankimg, this.x, this.y, 60, 40);
+	} else if (this.which == 2) {
+		ctx.drawImage(this.tankimg2, this.x, this.y, 60, 40);
+	}
+
 }
 
- Tank.prototype.moveDown = function () {
-   this.y += 2;
- }
+Tank.prototype.moveDown = function () {
+	this.y += 2;
+}
 
- Tank.prototype.moveRight = function () {
-   this.x += 2;
- }
+Tank.prototype.moveRight = function () {
+	this.x += 2;
+}
 
- Tank.prototype.moveLeft = function () {
-   this.x -= 2;
- }
- Tank.prototype.moveUp = function () {
-	 this.y -= 2;
- }
+Tank.prototype.moveLeft = function () {
+	this.x -= 2;
+}
+Tank.prototype.moveUp = function () {
+	this.y -= 2;
+}
+
+
+Bullet.prototype.draw = function () {
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, 2, 0, 2 * Math.PI)
+	ctx.fill();
+}
+
+Bullet.prototype.fire = function () {
+	this.x += 5;
+}
+
+Tank.prototype.shoot = function () {
+	if (this.ammo > 0) {
+		this.ammo--
+		this.bullets.push(new Bullet(this.x + 60, this.y + 21.5));
+	} else {
+		setTimeout(function () {
+			this.ammo = 15;
+		}.bind(this), 5000)
+	}
+}
+
+Tank.prototype.touchingwall = function () {
+	if (this.y <= -16.75) {
+		this.y += 20;
+		this.health -= 5;
+		if (this.health == 5) {
+			message = "Don't Run Into Walls";
+		}
+	}
+
+	if (this.y >= 115) {
+		this.y -= 20;
+		this.health -= 5;
+		if (this.health == 5) {
+			message = "Don't Run Into Walls";
+		}
+	}
+
+	if (this.x <= -16.75) {
+		this.x += 20;
+		this.health -= 5;
+		if (this.health == 5) {
+			message = "Don't Run Into Walls";
+		}
+	}
+
+	if (this.x >= 245) {
+		this.x -= 20;
+		this.health -= 5;
+		if (this.health == 5) {
+			message = "Don't Run Into Walls";
+		}
+	}
+}
 
 
 
- Bullet.prototype.draw = function () {
-	 ctx.arc(this.x,this.y,2,0,2 * Math.PI)
-	 ctx.fill();
- }
 
- Bullet.prototype.fire = function() {
-	 this.x += 5;
- }
-
- Tank.prototype.shoot = function () {
-	 this.bullets.push(new Bullet(this.x + 60, this.y + 21.5));
- }
-
- Tank.prototype.touchingwall = function () {
-	 if(this.y <= -16.75){
-		 this.y += 20;
-	 }
-
-	 if(this.y >= 115){
-		 this.y -= 20;
-	 }
-
-	 if(this.x <= -16.75){
-		 this.x += 20;
-	 }
-
-	 if(this.x >= 245) {
-		 this.x -= 20;
-	 }
-
-	 this.tank.draw()
- }
-
-function Game(){
-	this.tank = new Tank(10,10);
-	document.addEventListener("keydown",(e) => {
+function Game() {
+	this.tank = new Tank(10, 10, 1);
+	this.aI = new Tank(200, 50, 2)
+	document.addEventListener("keydown", (e) => {
 		var x = e.keyCode;
-		if(x == 83 || x == 40){
+		if (x == 83 || x == 40) {
 			this.tank.moveDown();
 			this.tank.draw();
-		}else if(x == 68 || x == 39){
-     this.tank.moveRight();
-		 this.tank.draw();
-		}else if(x == 87 || x == 38){
+		} else if (x == 68 || x == 39) {
+			this.tank.moveRight();
+			this.tank.draw();
+		} else if (x == 87 || x == 38) {
 			this.tank.moveUp();
 			this.tank.draw();
-		}else if(x == 65 || x == 37){
+		} else if (x == 65 || x == 37) {
 			this.tank.moveLeft();
 			this.tank.draw();
-		}else if(x == 32) {
-      this.tank.shoot()
-		} 
+		} else if (x == 32) {
+			this.tank.shoot()
+		}
 	});
 }
 
 Game.prototype.run = function () {
-	this.interval = setInterval(function(){
-		ctx.clearRect(0,0,canvas.width,canvas.height)
+	var interval = setInterval(function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.fillStyle = "black";
 		this.tank.draw();
-		this.tank.touchingwall()
-    for (var i = 0; i < this.tank.bullets.length; i++) {
+		this.tank.touchingwall();
+		this.aI.draw();
+		this.aI.touchingwall();
+		ctx.font = "9px Comic Sans MS";
+		ctx.fillStyle = "black";
+		ctx.textAlign = "center";
+		ctx.fillText(`Ammo: ${this.tank.ammo}`, 275, 10);
+		ctx.fillText(`Health: ${this.tank.health}`, 30, 10)
+		for (var i = 0; i < this.tank.bullets.length; i++) {
 			this.tank.bullets[i].draw();
-      this.tank.bullets[i].fire();
-    }
+			this.tank.bullets[i].fire();
+			if (this.tank.bullets[i].x < 0 || this.tank.bullets[i].x > canvas.width) {
+				this.tank.bullets.shift();
+			}
+
+		}
+
+		if (this.tank.health <= 0) {
+			setInterval(function () {
+				ctx.beginPath()
+				ctx.font = "20px Comic Sans MS";
+				ctx.fillStyle = "red";
+				ctx.textAlign = "center";
+				ctx.fillText(`${message}`, canvas.width / 2, canvas.height / 2);
+			})
+			clearInterval(interval)
+		}
+
+    enemyX = game.tank.x;
+
+		window.setInterval(function(){
+			this.aI.moveEnemy();
+		}.bind(this), 5000);
 	
-	if(this.tank.bullets[0].x < -60) {
-      this.tank.bullets.shift();  
-    }
 
-		
 
-	}.bind(this),100)
+    enemyY = game.tank.y;
+	}.bind(this), 100)
 }
 
-var game = new Game();
 
-game.run();
+
+ctx.font = "20px Comic Sans MS";
+ctx.fillStyle = "green";
+ctx.textAlign = "center";
+ctx.fillText(`Shut Up Samhith ${stage}`, canvas.width / 2, canvas.height / 2);
+ctx.fillStyle = "red";
+ctx.font = "10px Permanent Marker";
+ctx.fillText("Click to begin!", (canvas.width / 2) + 5, (canvas.height / 2) + 20)
+
+addEventListener("click", function () {
+	game.run();
+});
